375de527c75f29f29367c83ed1e4cc8c9efba213
Vynz <76029153+VincelyVynz@users.noreply.github.com>
2025-09-02 18:22:41 +0530

    More Bug fixing in LineCounter

--- log_reader.py
+++ log_reader.py
@@ -43,21 +43,24 @@ class LineCounter:
         try:
             self.content = open(self.filename, 'r')
         except FileNotFoundError:
+            self.content = None
             print(f"Could not find {self.filename}")
         self.line_count = 0
         self.word_count = 0
         self.processed = False
 
     def process_file(self):
-        line = self.content.readline().strip()
-        if line == "":
-            self.content.close()
-            self.processed = True
-            return self.line_count
-        else:
-            self.line_count += 1
-            self.word_count += len(line.split())
-            self.process_file()
+        if self.content is not None:
+            line = self.content.readline().strip()
+            if line == "":
+                self.content.close()
+                self.processed = True
+                return self.line_count
+            else:
+                self.line_count += 1
+                self.word_count += len(line.split())
+                self.process_file()
+        return None
 
     def get_line_count(self):
         if not self.processed:
