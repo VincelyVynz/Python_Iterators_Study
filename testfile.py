067dc0995e69476b75acadac570807279fa9ff1a
Vynz <76029153+VincelyVynz@users.noreply.github.com>
2025-09-03 10:40:09 +0530

    More Testing

--- testfile.py
+++ testfile.py
@@ -2,12 +2,12 @@ from counter import Counter
 from keyword_finder import KeywordFinder
 from linecounter import LineCounter
 
-story = KeywordFinder('textfile.txt', keyword="[ERROR]")
-
-story.print_keyword_line()
-
-
-# lc = LineCounter("textfile.txt")
+# story = KeywordFinder('textfile.txt', keyword="[ERROR]")
 #
-# lc.get_line_count()
-# lc.get_word_count()
+# story.print_keyword_line()
+
+
+lc = LineCounter("textfile.txt")
+
+lc.get_line_count()
+lc.get_word_count()
